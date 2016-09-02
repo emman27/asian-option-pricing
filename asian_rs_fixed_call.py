da8f2f11094c6714be68069686e81c56893f8fbf
Emman <eygohlolz@gmail.com>
2016-09-02 08:47:49 +0800

    Touch up a little

--- asian_rs_fixed_call.py
+++ asian_rs_fixed_call.py
@@ -2,12 +2,13 @@ from grid import Grid
 import math
 
 class AsianRSFixedCall:
-  def __init__(self, maxx, maxt, numx, numt, r):
+  def __init__(self, maxx, maxt, numx, numt, r, sigma):
     self.maxx = maxx
     self.T = maxt
     self.numx = numx
     self.numt = numt
     self.r = r
+    self.sigma = sigma
 
     self.dx = maxx / numx
     self.dt = maxt / numt
@@ -20,11 +21,17 @@ class AsianRSFixedCall:
     self.set_height_zero()
 
   def set_height_zero(self):
+    '''
+    Sets the boundary values for the FDS grid when the height is 0
+    '''
     for col in range(self.numt):
       time = col * self.dt
       self.grid.set_value_at(0, time, self.initial_zero_value_at_time(time))
 
   def initial_zero_value_at_time(self, time):
+    '''
+    Calculates the zero-height value of the FDS grid at given time @time
+    '''
     top = (1 - math.exp(-self.r * time))
     bottom = (self.r * self.T)
     return top / bottom
--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import numpy as np
 # Library for parsing command line arguments
 import sys
 
-import grid
-
 def main():
   '''
   Main method in the function
   Arguments to pass, in the following order:
-  @maxx: Maximum numeraire value of the stock at any point of time
+  @maxx: Maximum value of the stock at any point of time
   @maxt: Time to maturity of the stock, assuming current time t=0
-  @numx: Number of nonzero numeraire points of the stock
+  @numx: Number of nonzero price points of the stock
   @numt: Number of nonzero time points of monitoring
   Note that minimum price of a stock is simply 0
   '''
   maxx, maxt, numx, numt = sys.argv[1:] # First argument of argv is the script name
+
 
 if __name__ == "__main__":
   main()
