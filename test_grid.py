b7d65f4654928fd1b312a48178fd3be089e0f8aa
Emman <eygohlolz@gmail.com>
2016-09-07 13:20:58 +0800

    Fix unit tests for grid

--- test_grid.py
+++ test_grid.py
@@ -6,7 +6,7 @@ class TestGridMethods(unittest.TestCase):
     '''
     Ensures that matrices are created with the correct dimensions
     '''
-    grid = Grid(100, 99)
+    grid = Grid(1, 1, 100, 99)
     self.assertEqual(100, grid.get_raw_matrix().shape[0])
     self.assertEqual(99, grid.get_raw_matrix().shape[1])
 
@@ -14,14 +14,14 @@ class TestGridMethods(unittest.TestCase):
     '''
     Ensures that the retrieval method of Grid works
     '''
-    grid = Grid(50, 50)
+    grid = Grid(1, 1, 50, 50)
     self.assertEqual(0, grid.get_value(0, 34))
 
   def testSetValue(self):
     '''
     Ensures that can correctly set a value. Dependent on get_value to work as well
     '''
-    grid = Grid(10, 10)
+    grid = Grid(1, 1, 10, 10)
     grid.set_value(3, 3, 22)
     self.assertEqual(22, grid.get_value(3, 3))
 
@@ -29,7 +29,7 @@ class TestGridMethods(unittest.TestCase):
     '''
     Ensures that an Exception is raised if the retrieval is invalid
     '''
-    grid = Grid(1, 1)
+    grid = Grid(1, 1, 1, 1)
     with self.assertRaises(IndexError):
       grid.get_value(1, 1)
 
