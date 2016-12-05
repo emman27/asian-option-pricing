6fb370d35b3bca12ca3fe9e0b0d4018873113549
Emman <eygohlolz@gmail.com>
2016-12-05 16:08:06 +0800

    Add A and B matrices for column generation

--- asian_rs_fixed_call.py
+++ asian_rs_fixed_call.py
@@ -29,10 +29,37 @@ class AsianRSFixedCall(Option):
 
   # Methods to calculate the abstracted alpha and beta variables.
   # Math needs to be confirmed
-  def self.alpha(height):
+  def alpha(height):
     return .25 * self.sigma**2 * height**2 * self.dt
 
-  def self.beta(height):
-    return (height * self.r * self.dx + 1 / self.maxt) * (self.dt / (4 * self.dx))
+  def beta(height):
+    return (height * self.r * self.dx + 1 / self.maxt) * (self.deft / (4 * self.dx))
+
+  # The relation between two columns are given by
+  # R = (I-B)^-1 * A * L
+  # where they follow the original relation
+  # R = AL + BR, with
+  # A = [[1,0,0,0...]...[i-1th, ith, i+1th, 0, 0...], [0, i-1th, ith, i+1th, 0...]...[...0,0,0,1]]
+  def A_matrix():
+    A = numpy.matrix([[0] * numt] * numx, dtype = numpy.float64)
+    for i in range(1, numx - 1):
+      a = alpha(i)
+      b = beta(i)
+      A.itemset((i, i - 1), (a + b) * dt)
+      A.itemset((i, i), (1/dt - 2*a) * dt)
+      A.itemset((i, i + 1), (a - b) * dt)
+    return A
+
+  def B_matrix():
+    B = numpy.matrix([[0] * numt] * numx, dtype = numpy.float64)
+    B.itemset((0,0), 1)
+    B.itemset((numx - 1, numt - 1), 1)
+    for i in range(1, numx - 1):
+      a = alpha(i)
+      b = beta(i)
+      A.itemset((i, i - 1), (a + b) * dt)
+      A.itemset((i, i), (-2 * a) * dt)
+      A.itemset((i, i + 1), (a - b) * dt)
+    return B
 
 print(AsianRSFixedCall(3, 1, 300, 100, 0.02, 0.3).grid)
