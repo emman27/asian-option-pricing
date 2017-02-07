7bd0ee30e758ebe757363c2d079d33f97101e101
Emman <eygohlolz@gmail.com>
2017-02-07 15:30:56 +0800

    Add floating vecer to test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-formulae=( "asian_rs_fixed_call.py" "asian_vecer_fixed_call.py" "asian_dl_fixed_call.py" )
+formulae=( "asian_rs_fixed_call.py" "asian_vecer_fixed_call.py" "asian_dl_fixed_call.py" "asian_vecer_float_call.py" )
 
 for i in "${formulae[@]}"
 do
