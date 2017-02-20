061889601bd6cba21692b4a9424fcdf402e997d8
Emman <eygohlolz@gmail.com>
2017-02-20 16:34:41 +0800

    Adjust maxx to independent of xi_initial on account of it being too small

--- src/asian_rs_float_call.py
+++ src/asian_rs_float_call.py
@@ -5,7 +5,7 @@ import math
 class AsianRSFloatCall(FloatingCall):
     def __init__(self, maxt, numx, numt, r, sigma, initial_price, old_average, t0):
         super().__init__(maxt, numx, numt, r, sigma, initial_price, old_average, t0)
-        self.maxx = abs(self.xi_initial * 3)
+        self.maxx = 2 # In this case, the generic abs(self.xi_initial * 3) is too small
         self.dx = self.maxx * 2 / self.numx
         self.j0 = round((self.xi_initial + self.maxx) / self.dx)
         self.set_boundary_conditions()
@@ -51,20 +51,20 @@ class AsianRSFloatCall(FloatingCall):
 
 #     t0 = 0.1
 #     print('Expected: 9.87, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-    # print('Expected: 9.34, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-    # print('Expected: 8.85, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
-    #
-    # t0 = 0.3
-    # print('Expected: 10.72, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-    # print('Expected: 9.06, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-    # print('Expected: 7.61, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
-    #
-    # t0 = 0.7
-    # print('Expected: 11.19, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-    # print('Expected: 6.86, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-    # print('Expected: 3.85, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
-    #
-    # t0 = 0.9
-    # print('Expected: 10.40, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-    # print('Expected: 4.05, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-    # print('Expected: 1.03, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+#     print('Expected: 9.34, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+#     print('Expected: 8.85, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+
+#     t0 = 0.3
+#     print('Expected: 10.72, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
+#     print('Expected: 9.06, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+#     print('Expected: 7.61, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+
+#     t0 = 0.7
+#     print('Expected: 11.19, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
+#     print('Expected: 6.86, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+#     print('Expected: 3.85, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+
+#     t0 = 0.9
+#     print('Expected: 10.40, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
+#     print('Expected: 4.05, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+#     print('Expected: 1.03, Actual: ' + str(AsianRSFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
