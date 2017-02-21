689f4d481e90ff0f1c2cec81f2ca13d305de387c
Emman <eygohlolz@gmail.com>
2017-02-21 12:25:16 +0800

    Rewrite maxx calculation, but still wrong anyway

--- src/asian_new_vecer_float_call.py
+++ src/asian_new_vecer_float_call.py
@@ -5,9 +5,9 @@ import numpy
 class AsianNewVecerFloatCall(FloatingCall):
     def __init__(self, maxt, numx, numt, r, sigma, initial_price, old_average, t0):
         super().__init__(maxt, numx, numt, r, sigma, initial_price, old_average, t0)
-        self.j0 = round(numx/3)
-        self.dx = self.xi_initial / self.j0
-        self.maxx = self.dx * self.numx
+        self.maxx = self.xi_initial * 3
+        self.dx = self.maxx / self.numx
+        self.j0 = round(self.xi_initial / self.dx)
         self.set_boundary_conditions()
 
     def xi(self, s, t):
@@ -37,21 +37,21 @@ class AsianNewVecerFloatCall(FloatingCall):
         super().solve(lambda time: numpy.identity(self.numx + 1) - self.B_matrix(time), lambda time: self.A_matrix(time))
         return self.s0 * self.grid[self.j0, self.numt]
 
-# if __name__ == "__main__":
-#     # Constants
-#     numx = 200
-#     numt = 400
-#     maxt = 1
-#     r = 0.1
-#     s0 = 100
-#     sigma = 0.3
+if __name__ == "__main__":
+    # Constants
+    numx = 200
+    numt = 400
+    maxt = 1
+    r = 0.1
+    s0 = 100
+    sigma = 0.3
 
-#     t0 = 0.1
-#     print('Expected: 9.87, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-#     print('Expected: 9.35, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-#     print('Expected: 8.84, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+    t0 = 0.1
+    print('Expected: 9.87, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
+    print('Expected: 9.35, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+    print('Expected: 8.84, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
 
-#     t0 = 0.7
-#     print('Expected: 11.21, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
-#     print('Expected: 6.86, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
-#     print('Expected: 3.85, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
+    t0 = 0.7
+    print('Expected: 11.21, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 90, t0).solve()))
+    print('Expected: 6.86, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 100, t0).solve()))
+    print('Expected: 3.85, Actual: ' + str(AsianNewVecerFloatCall(maxt - t0, numx, numt, r, sigma, s0, 110, t0).solve()))
