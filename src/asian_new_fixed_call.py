6e5e4ef54dd4f12a7648041737ddbd30f8359934
Emman <eygohlolz@gmail.com>
2017-03-02 19:56:55 +0800

    Major modifications to improve new-vecer-float and new-fix

--- src/asian_new_fixed_call.py
+++ src/asian_new_fixed_call.py
@@ -5,7 +5,7 @@ import numpy
 class AsianNewFixedCall(FixedCall):
     def __init__(self, maxt, numx, numt, r, sigma, initial_price, strike):
         super().__init__(maxt, numx, numt, r, sigma, initial_price, strike)
-        self.maxx = abs(3 * self.xi_initial)
+        self.maxx = abs(5 * self.xi_initial)
         self.dx = self.maxx / self.numx
         self.j0 = self.numx + round(self.xi_initial / self.dx)
         self.set_boundary_conditions()
